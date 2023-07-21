159d75be423227c677be34f617c5d6343c139ee4
Lorena Sales <lorenassales@gmail.com>
2023-07-21 20:12:09 +0000

    Included column qty rental days

--- models/stage/erp/stg_erp__rentals.sql
+++ models/stage/erp/stg_erp__rentals.sql
@@ -4,15 +4,23 @@ with
         from {{ source('erp', 'rental') }}
     )
 
-    , transforms as (
+    , transforms_01 as (
         select
             cast(rental_id as int) as rental_id
             , cast(staff_id as int) as staff_id            
             , cast(inventory_id as int) as inventory_id
             , cast(customer_id as int) as customer_id
-            , cast(rental_date as datetime) as rental_date
-            , cast(return_date as datetime) as return_date            
+            , cast(left(cast(rental_date as string), 10) as date) as rental_date
+            , cast(left(cast(return_date as string), 10) as date) as return_date          
         from rentals
     )
-select *
-from transforms
+
+    , transforms_02 as (
+        select 
+            *
+            , cast(date_diff(return_date, rental_date, day) as int) as qty_rental_days 
+        from transforms_01
+    )
+select
+    *
+from transforms_02
