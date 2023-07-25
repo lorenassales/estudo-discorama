8520ac5d7d7b8295fb4b33dede893a14d425daf8
Lorena Sales <lorenassales@gmail.com>
2023-07-25 15:57:47 +0000

    add actors on fct

--- models/marts/dim_film_actors.sql
+++ models/marts/dim_film_actors.sql
@@ -11,14 +11,14 @@ with
 
     , dim_film_actors as (
         select
-            fa.film_id
-            , a.actor_id
-            , a.actor_name
+            fa.film_id            
+            , string_agg(a.actor_name, ', ') as group_actor_name
         from stg_erp__film_actors fa
         left join stg_erp__actors a on
             fa.actor_id = a.actor_id
+        group by fa.film_id
     )
 select
-    {{ dbt_utils.generate_surrogate_key(['film_id', 'actor_id']) }} as film_actor_sk
+    {{ dbt_utils.generate_surrogate_key(['film_id', 'group_actor_name']) }} as film_actor_sk
     , *
 from dim_film_actors
